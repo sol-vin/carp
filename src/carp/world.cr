dc8e18a370abf544881843b7890d25726f83b351
Ian Rash <redcodefinal@gmail.com>
2023-09-22 09:12:54 -0700

    More werk

--- src/carp/world.cr
+++ src/carp/world.cr
@@ -22,30 +22,65 @@ class Carp::World
 
   @bodies = [] of Body
 
-  
+  getter accumulator : Float64 = 0.0_f64
+  getter timestamp : Float64 = 0.0_f64
+
+  record(
+    ContactCache,
+    key : BodyPair,
+    value : Collision
+  )
+
+  pre_step_hook = ->(cache : ContactCache) {false}
+  post_step_hook = ->(cache : ContactCache) {false}
+
+
+  @cache : Array(ContactCache) = [] of ContactCache
 
   def initialize(@gravity = DEFAULT_GRAVITY, cell_size = DEFAULT_CELL_SIZE)
-    spatial_hash = SpatialHash.new(cell_size)
+    @spatial_hash = SpatialHash.new(cell_size)
   end
 
   def clear
+    @bodies.clear
+    @spatial_hash.clear
   end
 
   def pre_step
   end
 
-  def step
+  def step(delta_time : Float64)
+    raise "Invalid delta_time: #{delta_time}" if delta_time < 0
   end
 
   def post_step
+    pre_step
+
+    @cache.size.times do |c|
+      pre_step_hook.call(c)
+    end
+
+    #TODO: Implement BodyTOGravity
+
+    post_step
   end
 
   def update
   end
 
   def <<(body : Body)
+    bodies << body
   end
 
   def [](index : Int32)
+    bodies[index]
+  end
+
+  def size
+    bodies.size
+  end
+
+  def delete(body : Body)
+    bodies.delete(body)
   end
 end
